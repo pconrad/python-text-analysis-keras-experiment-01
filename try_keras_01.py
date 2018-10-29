58fbb69f0f5b6cc4336fe3cf4ed661a929900b30
Phill Conrad <pconrad@cs.ucsb.edu>
2018-10-29 10:00:09 -0700

    add Python code from tytorial and rename data files to match tutorial

---
+++ try_keras_01.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+filepath_dict = {'yelp':   'data/sentiment_analysis/yelp_labelled.txt',
+                 'amazon': 'data/sentiment_analysis/amazon_cells_labelled.txt',
+                 'imdb':   'data/sentiment_analysis/imdb_labelled.txt'}
+
+df_list = []
+for source, filepath in filepath_dict.items():
+    df = pd.read_csv(filepath, names=['sentence', 'label'], sep='\t')
+    df['source'] = source  # Add another column filled with the source name
+    df_list.append(df)
+
+df = pd.concat(df_list)
+print(df.iloc[0])
